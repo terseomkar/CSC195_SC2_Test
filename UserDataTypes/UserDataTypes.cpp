a3b5daa02a449b16f23d5d2d34b8ca95c747cb2d
terseomkar <oterse@neumont.edu>
2025-03-25 16:23:52 -0600

    Added UserDataTypes

---
+++ UserDataTypes/UserDataTypes.cpp
@@ -0,0 +1,45 @@
+// UserDataTypes.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+class Sandwich
+{
+public:
+    char name[32];
+    float price;
+    bool isHot;
+};
+
+enum Difficulty
+{
+    Easy,
+    Medium,
+    Hard
+};
+
+
+int main()
+{
+    Sandwich turkey;
+    turkey.price = 4.99f;
+    turkey.isHot = true;
+
+    std::cout << sizeof(Sandwich) << std::endl;
+    std::cout << sizeof(Difficulty);
+
+    Difficulty difficulty;
+    difficulty = Medium;
+
+
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
